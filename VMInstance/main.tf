a3133b5c6e41e57ee3b3a2c6f4a92be52678ecc2
kalkirs <92350036+kalkirs@users.noreply.github.com>
2022-11-16 17:49:42 +0530

    Update main.tf

--- VMInstance/main.tf
+++ VMInstance/main.tf
@@ -1,7 +1,7 @@
 # This is the provider used to spin up the gcloud instance
 provider "google" {
  
-  project = "mi-dev-lab"
+  project = "murthylab"
   credentials = file("terraform.json")
   region  = "us-east1"
   
